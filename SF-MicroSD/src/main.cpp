68dafaaba17bad10502eec4679b44ddf8682a15a
darin <dpadlewski@gmail.com>
2022-02-20 17:36:06 -0700

    SD experiments doc and code

--- SF-MicroSD/src/main.cpp
+++ SF-MicroSD/src/main.cpp
@@ -1,183 +1,80 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <SD.h>
+#include "disk.h"
+#include "ir_10-150cm.h"
+#include "ir_pololu_prox.h"
 
-File fd;
-const uint8_t BUFFER_SIZE = 20;
-char fileName[] = "demoFile.txt"; // SD library only supports up to 8.3 names
-char buff[BUFFER_SIZE+2] = "";  // Added two to allow a 2 char peek for EOF state
-uint8_t index = 0;
+IR ir = init(A0, 5);
+byte run = 1;
+const char eol[2] = "\n";
+char f_runs[] = "runs.txt";
+char f_test[] = "test.txt";
+char f_log[] = "data.log";
+byte idx = 0;
+static const byte MAX = B11111111; // 255
+byte buff[MAX];
+ProximitySensor prox = init(DD6, DD5, true);
 
-const uint8_t chipSelect = 8;
-const uint8_t cardDetect = 9;
-
-enum states: uint8_t { NORMAL, E, EO };
-uint8_t state = NORMAL;
-
-bool alreadyBegan = false;  // SD.begin() misbehaves if not first call
-
-void initializeCard(void);
-void readByte(void);
-void flushBuffer(void);
-void eof(void);
+void updateBuffer(void);
+void setRuns(void);
+void dumpBuffer(void);
 
 void setup() {
   Serial.begin(57600);
-  while (!Serial);  // Wait for serial port to connect (ATmega32U4 type PCBAs)
-
-  // Note: To satisfy the AVR SPI gods the SD library takes care of setting
-  // SS_PIN as an output. We don't need to.
+  while (!Serial);  // Wait for serial port to connect
+  // The SD library takes care of setting SS_PIN as an output.
   pinMode(cardDetect, INPUT);
-
   initializeCard();
+  setRuns();
+  testWrite(f_test);
 }
 
 void loop() {
-  // Make sure the card is still present
-  if (!digitalRead(cardDetect))
-  {
-    initializeCard();
-  }
+  read(&ir);
+  if(ir.cm < 20) setOff(&prox);
+  else setOn(&prox);
+  testRead(f_test);
+  Serial.print("Run #: "); Serial.println(run);
+  Serial.print("IR: ");Serial.print(ir.volts);Serial.print("v | ");Serial.print(ir.cm);Serial.println(" cm");
+  update(&prox);
+  Serial.println(prox.blocked ? "Blocked" : "Open");
+  updateBuffer();
+  delay(2500);
+}
 
-  if (Serial.available() > 0)
-  {
-    readByte();
+void updateBuffer(void) {
+  static const size_t writeSize = 1 + sizeof(ir.volts) + sizeof(ir.cm) + sizeof(eol);
+  if((idx + writeSize) >= MAX) dumpBuffer();
+  buff[idx++] = run;
+  size_t size = sizeof(ir.volts);
+  memcpy(buff, (uint8_t*) &ir.volts, size);
+  idx += size;
+  size = sizeof(ir.cm);
+  memcpy(buff, (uint8_t*) &ir.cm, size);
+  idx += size;
+  memcpy(buff, eol, 2);
+  Serial.print("Buffer size: ");
+  Serial.print(idx);
+  Serial.print(" | sizeof: ");
+  Serial.println(sizeof(buff));
+}
 
-    if (index == BUFFER_SIZE)
-    {
-      flushBuffer();  // Write full buffer to µSD card
+void setRuns(void){
+  byte buf[] = {0};
+  if(SD.exists(f_runs)){
+    uint16_t bytesRead;
+    readFromFile(f_runs, buf, 1, 0, &bytesRead);
+    if(bytesRead > 0){
+      run += buf[0];
     }
   }
+  buf[0] = run;
+  overwriteToFile(f_runs, buf, 1);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// Do everything from detecting card through opening the demo file
-////////////////////////////////////////////////////////////////////////////////
-void initializeCard(void)
-{
-  Serial.print(F("Initializing SD card..."));
-
-  // Is there even a card?
-  if (!digitalRead(cardDetect))
-  {
-    Serial.println(F("No card detected. Waiting for card."));
-    while (!digitalRead(cardDetect));
-    delay(250); // 'Debounce insertion'
-  }
-
-  // Card seems to exist.  begin() returns failure
-  // even if it worked if it's not the first call.
-  if (!SD.begin(chipSelect) && !alreadyBegan)  // begin uses half-speed...
-  {
-    Serial.println(F("Initialization failed!"));
-    initializeCard(); // Possible infinite retry loop is as valid as anything
-  }
-  else
-  {
-    alreadyBegan = true;
-  }
-  Serial.println(F("Initialization done."));
-
-  Serial.print(fileName);
-  if (SD.exists(fileName))
-  {
-    Serial.println(F(" exists."));
-  }
-  else
-  {
-    Serial.println(F(" doesn't exist. Creating."));
-  }
-
-  Serial.print("Opening file: ");
-  Serial.println(fileName);
-
-  Serial.println(F("Enter text to be written to file. 'EOF' will terminate writing."));
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-// This function is called after the EOF command is received. It writes the
-// remaining unwritten data to the µSD card, and prints out the full contents
-// of the log file.
-////////////////////////////////////////////////////////////////////////////////
-void eof(void)
-{
-  index -= 3; // Remove EOF from the end
-  flushBuffer();
-
-  // Re-open the file for reading:
-  fd = SD.open(fileName);
-  if (fd)
-  {
-    Serial.println("");
-    Serial.print(fileName);
-    Serial.println(":");
-
-    while (fd.available())
-    {
-      Serial.write(fd.read());
-    }
-  }
-  else
-  {
-    Serial.print("Error opening ");
-    Serial.println(fileName);
-  }
-  fd.close();
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Write the buffer to the log file. If we are possibly in the EOF state, verify
-// that to make sure the command isn't written to the file.
-////////////////////////////////////////////////////////////////////////////////
-void flushBuffer(void)
-{
-  fd = SD.open(fileName, FILE_WRITE);
-  if (fd) {
-    switch (state)  // If a flush occurs in the 'E' or the 'EO' state, read more to detect EOF
-    {
-    case NORMAL:
-      break;
-    case E:
-      readByte();
-      readByte();
-      break;
-    case EO:
-      readByte();
-      break;
-    }
-    fd.write(buff, index);
-    fd.flush();
-    index = 0;
-    fd.close();
-  }
-}
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Reads a byte from the serial connection. This also maintains the state to
-// capture the EOF command.
-////////////////////////////////////////////////////////////////////////////////
-void readByte(void)
-{
-  byte byteRead = Serial.read();
-  Serial.write(byteRead); // Echo
-  buff[index++] = byteRead;
-
-  // Must be 'EOF' to not get confused with words such as 'takeoff' or 'writeoff'
-  if (byteRead == 'E' && state == NORMAL)
-  {
-    state = E;
-  }
-  else if (byteRead == 'O' && state == E)
-  {
-    state = EO;
-  }
-  else if (byteRead == 'F' && state == EO)
-  {
-    eof();
-    state = NORMAL;
-  }
+void dumpBuffer(void) {
+  Serial.println("Writeing to File");
+  writeToFile(f_log, buff, idx);
+  idx = 0;
+  Serial.print("File in bytes: ");
+  Serial.println(getSize(f_log));
 }
